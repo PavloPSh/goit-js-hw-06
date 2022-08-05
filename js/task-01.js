c3027ff10517815ea2233cff6a32f6cb70731bc9
Pavlo Psheborovskyi <pavlopshe@gmail.com>
2022-08-05 20:33:40 +0300

    made edits

--- js/task-01.js
+++ js/task-01.js
@@ -1,17 +1,29 @@
 const categoryEl = document.querySelector('#categories');  
 console.log('Number of categories:', categoryEl.childElementCount);
 
+const itemEl = document.querySelectorAll('.item');
 
-const firstItemEl = categoryEl.children[0];
+itemEl.forEach(item =>{
+console.log('Category:', `${item.querySelector('h2').textContent}`);
+console.log('Elements:', `${item.querySelectorAll("li").length}`);
+});
 
-console.log('Category:', firstItemEl.firstElementChild.textContent);
-console.log('Elements:', firstItemEl.lastElementChild.childElementCount);
 
-const secondItemEl = categoryEl.children[1];
 
-console.log('Category:', secondItemEl.firstElementChild.textContent);
-console.log('Elements:', secondItemEl.lastElementChild.childElementCount);
 
-const thirdItemEl = categoryEl.children[2];
-console.log('Category:', thirdItemEl.firstElementChild.textContent);
-console.log('Elements:', thirdItemEl.lastElementChild.childElementCount);
+
+// const firstItemEl = categoryEl.children[0];
+
+// console.log('Category:', firstItemEl.firstElementChild.textContent);
+// console.log('Elements:', firstItemEl.lastElementChild.childElementCount);
+
+// const secondItemEl = categoryEl.children[1];
+
+// console.log('Category:', secondItemEl.firstElementChild.textContent);
+// console.log('Elements:', secondItemEl.lastElementChild.childElementCount);
+
+// const thirdItemEl = categoryEl.children[2];
+// console.log('Category:', thirdItemEl.firstElementChild.textContent);
+// console.log('Elements:', thirdItemEl.lastElementChild.childElementCount);
+
+
--- js/task-05.js
+++ js/task-05.js
@@ -2,4 +2,4 @@ const input = document.querySelector('#name-input');
 const output = document.querySelector('#name-output');
 
 input.addEventListener('input', (event) =>
-{output.textContent = event.currentTarget.value});
+{output.textContent = event.currentTarget.value || 'Anonymous'});
